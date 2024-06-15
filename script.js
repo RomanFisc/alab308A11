e1a9753b8a6359322ec157b176502fd624f12bb8
RomanFisc <romanfisc@gmail.com>
2024-06-15 00:48:41 -0400

    1

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 // Part 1 
 // declare a variable 
-let counter = 0
-//create function that increments the variable and calls itself recursively 
-function count(){
-  counter += 1
-  count()
+// let counter = 0
+
+// function count(){
+//   counter += 1
+//   count()
+// }
+
+// try {
+//   count()
+// } catch (error) {
+//   console.log("Error message:", error)
+//   console.log("Function count:", counter)
+// }
+
+
+//part 2 trampolining 
+// write a function that flattens an array of nested arrafys no matter how deeply nested they are: 
+
+const data = [
+  [1,2,3],
+  [4,[5,6,[9,8]]],
+  10
+];
+
+function flatten(arr) {
+  let flat = []
+  for(let i = 0; i < arr.length; i++){
+    if (Array.isArray(arr[i])) {
+      flat = flat.concat(flatten(arr[i]))
+    } else {
+      flat.push(arr[i])
+    }
+  }
+  return flat
 }
 
-try {
-  count()
-} catch (error) {
-  console.log("Error message", error)
-  console.log("Function count", counter)
-}
+console.log(
+  flatten(data)
+)
+
+// converting that to trampoline function
